e644342eceff926ef66cf04ca29abf7aa3dfa25a
Natalia Zueva <zueva_natalia@mail.ru>
2024-04-08 11:06:13 +0500

    Password hashing

--- users/views.py
+++ users/views.py
@@ -7,28 +7,24 @@ from users.serializers import UserSerializer
 
 class UserCreateAPIView(generics.CreateAPIView):
     """
-    Контроллер для создания пользователей
+    Регистрация нового пользователя
     """
     serializer_class = UserSerializer
-    queryset = User.objects.all()
 
-    def post(self, request, *args, **kwargs):
+    def create(self, request, *args, **kwargs):
+        """Переопределение метода для сохранения хешированного пароля в БД"""
         serializer = UserSerializer(data=request.data)
-        data = {}
-        if serializer.is_valid():
-            serializer.save()
-            data['response'] = True
-            return Response(data, status=status.HTTP_200_OK)
-        else:
-            data = serializer.errors
-            return Response(data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
 
-    def perform_create(self, request, *args, **kwargs):
-        data = request.data
-        password = data.get('password')
-        user = User.object.get(email=data.get('email'))
+        password = serializer.data["password"]
+        user = User.objects.get(pk=serializer.data["id"])
         user.set_password(password)
+        user.is_active = True
         user.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
 class UserUpdateAPIView(generics.UpdateAPIView):
